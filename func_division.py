594626881404890b3ef474082a15409215399068
Maks Korystov <corystov.maks@yandex.ru>
2023-12-04 23:50:43 +0300

    Change func_division.py AND Add .idea/

--- func_division.py
+++ func_division.py
@@ -1,4 +1,9 @@
 # функция деления
 def division_func(x: int, y: int):
-    if y != 0:
+    try:
         return x / y
+    except ZeroDivisionError:
+        return "Division by Zero!!!"
+
+
+print(division_func(3, 3))
